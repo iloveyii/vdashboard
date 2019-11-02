f714c7c3e9d3fbb0a61c8ea3140136abf9aea847
Hazrat Ali <iloveyii@yahoo.com>
2019-11-02 15:00:58 +0100

    Build - aws

---
+++ react/build/precache-manifest.05e3f0ae936ace0f7d816c366fdb6c72.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.42ac5946.js"
+  },
+  {
+    "revision": "076779b9d6ee5f53c69e",
+    "url": "/static/js/main.076779b9.chunk.js"
+  },
+  {
+    "revision": "8e4f52d87991b9243a32",
+    "url": "/static/js/2.8e4f52d8.chunk.js"
+  },
+  {
+    "revision": "076779b9d6ee5f53c69e",
+    "url": "/static/css/main.87091116.chunk.css"
+  },
+  {
+    "revision": "6fb3d7507dc93f5d1ab8d46a064f67f7",
+    "url": "/index.html"
+  }
+];
