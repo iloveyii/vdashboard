5a3e243cb91ac16e65a0557555aafebcb1c18030
Hazrat Ali <iloveyii@yahoo.com>
2019-11-25 19:37:36 +0100

    Added env to react

--- react/src/common/constants.js
+++ react/src/common/constants.js
@@ -1,4 +1,5 @@
-const apiServer = 'http://mobile-server.softhem.se:8090';
+const apiServer = process.env.REACT_APP_serverIp ? process.env.REACT_APP_serverIp : 'http://mobile-server.softhem.se:8090';
+console.log('ENV:', apiServer);
 
 export  {
     apiServer,
